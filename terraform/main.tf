0953ef6de33b299f3475ac3daaa6d074820cb4e9
Artem <jokerkiller1111@gmail.com>
2025-10-21 12:03:36 +0300

    task completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,56 @@ terraform {
 }
 
 provider "docker" {}
+
+# Create a Docker network
+resource "docker_network" "app_network" {
+  name = "${var.project_name}_network"
+}
+
+# Create a Docker volume
+resource "docker_volume" "app_volume" {
+  name = "${var.project_name}_volume"
+}
+
+# PHP-FPM container
+resource "docker_container" "php" {
+  name  = "${var.project_name}_php"
+  image = "php:8-fpm"
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  volumes {
+    volume_name    = docker_volume.app_volume.name
+    container_path = "/var/www/html"
+  }
+
+  env = [
+    "APP_ENV=${var.app_env}"
+  ]
+}
+
+# Nginx container
+resource "docker_container" "nginx" {
+  name  = "${var.project_name}_nginx"
+  image = "nginx:latest"
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  volumes {
+    volume_name    = docker_volume.app_volume.name
+    container_path = "/var/www/html"
+  }
+
+  ports {
+    internal = 80
+    external = var.host_port
+  }
+
+  env = [
+    "APP_ENV=${var.app_env}"
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
-# TODO
+output "nginx_container_name" {
+  value = docker_container.nginx.name
+}
+
+output "php_container_name" {
+  value = docker_container.php.name
+}
+
+output "healthz_url" {
+  value = "http://localhost:${var.host_port}/healthz"
+}
